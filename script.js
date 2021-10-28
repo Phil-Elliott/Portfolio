47449867ccecd1bba1276708c81cf9e54d99d655
Phil_Elliott <phillippelliott@gmail.com>
2021-10-28 13:19:11 -0400

    changed margin

---
+++ script.js
@@ -0,0 +1,10 @@
+const projects = document.querySelector('.projects-bttn')
+const resume = document.querySelector('.resume-bttn')
+
+console.log(pageYOffset)
+
+projects.addEventListener('click', () => {
+	window.scrollTo(0, 1507);
+})
+
+//1507
